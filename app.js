fb03379b9b417a5d6ef4c41f8a879fe2371d1cb2
Emmanuel Omene <e.omene2012@gmail.com>
2023-05-04 19:06:56 +0200

    Update app.js in PR #23

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-  const darkModeToggle = document.getElementById('darkModeToggle');
-  const body = document.querySelector('body');
-  darkModeToggle.addEventListener('click', function () {
-    body.classList.toggle('dark-mode');
-    body.classList.toggle('light-mode');
-  });
-});
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
